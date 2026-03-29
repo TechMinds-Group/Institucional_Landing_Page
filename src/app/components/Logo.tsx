cd6cbc08117f413cad2ec1e94bcb1209959e854a
victor-souza-dev <victor.souza2210@gmail.com>
2026-03-29 18:22:51 -0300

    Revert "Update files from Figma Make"
    This reverts commit 0e5903b7f0440d320fb767b6b522f54ff07b8aa8.

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,5 +1,5 @@
 export function Logo({ className = "w-2 h-2" }: { className?: string }) {
   return (
-    <img className={className} src="../../../guidelines/assets/logo_techminds.png" />
+    <img className={className} src="/logo_techminds.png" />
   );
 }
